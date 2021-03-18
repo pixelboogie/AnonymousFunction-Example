9084fde4144227de920d6d8d77ebd0fe7eb2a0ec
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-18 10:17:03 -0700

    Add files via upload

---
+++ AnonFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnonFunction : MonoBehaviour
+{
+
+    public delegate void TestDelegate();
+    private TestDelegate testDelegateFunction;
+
+    void Start()
+    {
+                                        // anonymous function
+        testDelegateFunction = delegate () { Debug.Log("Anonymous method");};
+        testDelegateFunction();
+        
+    }
+
+}
